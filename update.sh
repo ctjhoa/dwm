17f54674e85909df0732808e0a702a5feb0e97c7
Camille TJHOA <camille@contract-live.com>
2016-03-21 09:43:19 +0100

    Invert diff

--- update.sh
+++ update.sh
@@ -6,6 +6,6 @@ curl https://projects.archlinux.org/svntogit/community.git/plain/trunk/config.h?
 curl https://projects.archlinux.org/svntogit/community.git/plain/trunk/dwm.desktop?h=packages/dwm --output dwm.desktop
 curl https://projects.archlinux.org/svntogit/community.git/plain/trunk/dwm.install?h=packages/dwm --output dwm.install
 
-vimdiff PKGBUILD.def PKGBUILD
-vimdiff config.h.def config.h
+vimdiff PKGBUILD PKGBUILD.def
+vimdiff config.h config.h.def
 updpkgsums
