0251b1f94bfe29c8f6af4e68e6e7c4bfefd7a143
Camille TJHOA <camille@contract-live.com>
2016-09-24 12:56:30 +0200

    Fix url

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Fetch sources 
-curl https://projects.archlinux.org/svntogit/community.git/plain/trunk/PKGBUILD?h=packages/dwm --output PKGBUILD.def
-curl https://projects.archlinux.org/svntogit/community.git/plain/trunk/config.h?h=packages/dwm --output config.h.def
-curl https://projects.archlinux.org/svntogit/community.git/plain/trunk/dwm.desktop?h=packages/dwm --output dwm.desktop
-curl https://projects.archlinux.org/svntogit/community.git/plain/trunk/dwm.install?h=packages/dwm --output dwm.install
+curl -sSfL https://git.archlinux.org/svntogit/community.git/plain/trunk/PKGBUILD?h=packages/dwm --output PKGBUILD.def
+curl -sSfL https://git.archlinux.org/svntogit/community.git/plain/trunk/config.h?h=packages/dwm --output config.h.def
+curl -sSfL https://git.archlinux.org/svntogit/community.git/plain/trunk/dwm.desktop?h=packages/dwm --output dwm.desktop
+curl -sSfL https://git.archlinux.org/svntogit/community.git/plain/trunk/dwm.install?h=packages/dwm --output dwm.install
 
 vimdiff PKGBUILD PKGBUILD.def
 vimdiff config.h config.h.def
