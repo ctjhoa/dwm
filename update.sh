1a21a9cba6ad2d68bfac2a7ec12d643a625b385b
Camille TJHOA <camille.tjhoa@outlook.com>
2019-06-10 07:53:23 +0000

    Fix archlinux urls

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Fetch sources 
-curl -sSfL https://git.archlinux.org/svntogit/community.git/plain/trunk/PKGBUILD?h=packages/dwm --output PKGBUILD.def
-curl -sSfL https://git.archlinux.org/svntogit/community.git/plain/trunk/config.h?h=packages/dwm --output config.h.def
-curl -sSfL https://git.archlinux.org/svntogit/community.git/plain/trunk/dwm.desktop?h=packages/dwm --output dwm.desktop
-curl -sSfL https://git.archlinux.org/svntogit/community.git/plain/trunk/dwm.install?h=packages/dwm --output dwm.install
+curl -sSfL https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=dwm --output PKGBUILD.def
+curl -sSfL https://aur.archlinux.org/cgit/aur.git/plain/config.h?h=dwm --output config.h.def
+curl -sSfL https://aur.archlinux.org/cgit/aur.git/plain/dwm.desktop?h=dwm --output dwm.desktop
+curl -sSfL https://aur.archlinux.org/cgit/aur.git/plain/dwm.install?h=dwm --output dwm.install
 
 vimdiff PKGBUILD PKGBUILD.def
 vimdiff config.h config.h.def
